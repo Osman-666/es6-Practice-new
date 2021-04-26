2ba49f91b0f5a506df52b07103094ff65e8d59e6
Osman Ali <yasinosman666@gmail.com>
2021-04-26 11:19:53 +0600

    finish variable declaration

---
+++ default.js
@@ -0,0 +1,5 @@
+function add(num1,num2=50) {
+    return num1 + num2;
+}
+const result = add(151,49);
+console.log(result);
---
+++ letconst.js
@@ -0,0 +1,16 @@
+const wifi = 'someone';
+//console.log(wifi);
+const nums = [12,48];
+nums[1]=84;
+nums.push(12);
+//console.log(nums);
+
+let patientName = "Rahim chacha";
+patientName = "Rahima khala"
+console.log(patientName);
+
+let sum = 0;
+for(let i=0;i<=10;i++){
+    sum=sum+i;
+}
+console.log(i);
