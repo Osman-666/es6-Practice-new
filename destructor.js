f59642697ca7723e010ead8d19a2d155953f6502
Osman Ali <yasinosman666@gmail.com>
2021-04-29 13:01:31 +0600

    finished es6 module

---
+++ destructor.js
@@ -0,0 +1,11 @@
+// const person= {name: "sajid", age: 20, work: "student", gfName: "mithila", friends:['osman','mehrab','sumon']}
+// const gfName = person.gfName;
+// const work = person.work;
+
+// const {gfName,name,work,friends}=person
+
+// console.log(gfName,work,name,friends);
+
+const friends =['nayon', 'raihan', 'antor', 'tanmoy', 'mehrab'];
+const [firstFriend,second,...sobayBondhu] = friends;
+console.log(sobayBondhu);
